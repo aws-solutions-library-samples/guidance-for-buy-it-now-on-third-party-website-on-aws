d0378c1409d181463d386236ee38a62a2d4a408d
Vijay Prince <vjprince@amazon.com>
2023-03-13 12:10:51 -0600

    Add cdk-nag with suppressions

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 
 import aws_cdk as cdk
 from aws_cdk import Aspects
-from cdk_nag import AwsSolutionsChecks
+from cdk_nag import (
+    AwsSolutionsChecks,
+    NagSuppressions
+)
 
 from guidance_for_buy_it_now_on_third_party_website_on_aws.guidance_for_buy_it_now_on_third_party_website_on_aws_stack import GuidanceForBuyItNowOnThirdPartyWebsiteOnAwsStack
 #from guidance_for_buy_it_now_on_third_party_website_on_aws.guidance_mock_stack import RootStack
@@ -10,8 +13,40 @@ from guidance_for_buy_it_now_on_third_party_website_on_aws.guidance_mock_stack i
 
 
 app = cdk.App()
-GuidanceForBuyItNowOnThirdPartyWebsiteOnAwsStack(app, "guidance-for-buy-it-now-on-third-party-website-on-aws")
-MockStack(app)
+buy_it_now_stack = GuidanceForBuyItNowOnThirdPartyWebsiteOnAwsStack(app, "guidance-for-buy-it-now-on-third-party-website-on-aws")
+third_party_stack = MockStack(app)
 #RootStack(app)
-##Aspects.of(app).add(AwsSolutionsChecks())
+Aspects.of(app).add(AwsSolutionsChecks(verbose=True))
+NagSuppressions.add_stack_suppressions(
+    stack=third_party_stack, 
+    suppressions=[
+        {"id":"AwsSolutions-APIG1", "reason":"Sample Code"},
+        {"id":"AwsSolutions-APIG2", "reason":"Sample Code"},
+        {"id":"AwsSolutions-APIG4", "reason":"Sample Code"},
+        {"id":"AwsSolutions-APIG6", "reason":"Sample Code"},
+        {"id":"AwsSolutions-COG4", "reason":"Sample Code"},
+        {"id":"AwsSolutions-IAM4", "reason":"Sample Code"},
+        # Suppress warnings
+        {"id":"AwsSolutions-APIG3", "reason":"Sample Code"},
+        {"id":"AwsSolutions-DDB3", "reason":"Sample Code"},
+    ]
+)
+NagSuppressions.add_stack_suppressions(
+    stack=buy_it_now_stack, 
+    suppressions=[
+        {"id":"AwsSolutions-APIG1", "reason":"Sample Code"},
+        {"id":"AwsSolutions-APIG2", "reason":"Sample Code"},
+        {"id":"AwsSolutions-APIG4", "reason":"Sample Code"},
+        {"id":"AwsSolutions-APIG6", "reason":"Sample Code"},
+        {"id":"AwsSolutions-COG4", "reason":"Sample Code"},
+        {"id":"AwsSolutions-IAM4", "reason":"Sample Code"},
+        {"id":"AwsSolutions-IAM5", "reason":"Sample Code"},
+        {"id":"AwsSolutions-SNS2", "reason":"Sample Code"},
+        {"id":"AwsSolutions-SNS3", "reason":"Sample Code"},
+        {"id":"AwsSolutions-SF2", "reason":"Sample Code"},
+        # Suppress warnings
+        {"id":"AwsSolutions-APIG3", "reason":"Sample Code"},
+        {"id":"AwsSolutions-DDB3", "reason":"Sample Code"},
+    ]
+)
 app.synth()
